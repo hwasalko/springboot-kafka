2fe101c22ff00f071ef2ad473d456ffaac67279d
Lee Dongjin <2dong0711@hanmail.net>
2020-12-25 18:50:45 +0900

    ㅈ

--- src/main/java/com/hwasalko/springbootkafka/rest/kafka/controller/KafkaController.java
+++ src/main/java/com/hwasalko/springbootkafka/rest/kafka/controller/KafkaController.java
@@ -2,8 +2,6 @@ package com.hwasalko.springbootkafka.rest.kafka.controller;
 
 import com.hwasalko.springbootkafka.rest.kafka.service.KafkaService;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
